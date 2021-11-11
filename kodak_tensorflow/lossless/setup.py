af2b859161524b4c94da0959006f4503395fd60a
Arpith G Naik <arpithbdk6@gmail.com>
2021-11-11 22:27:54 +0530

    modified

--- kodak_tensorflow/lossless/setup.py
+++ kodak_tensorflow/lossless/setup.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         os.path.join(path_to_cplusplus, 'LosslessCoder.cpp'),
         os.path.join(path_to_cplusplus, 'compression.cpp')
     ]
-    ext = Extension('/kaggle/temp/Autoencoder/kodak_tensorflow/lossless/interface_cython.pyx',
+    ext = Extension('interface_cython',
                     sources=sources,
                     language='c++',
                     extra_compile_args=['-std=c++11']
