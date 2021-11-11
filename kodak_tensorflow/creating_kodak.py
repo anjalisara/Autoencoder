08a4ebb96138fcd9d0a0988de1910113506f14a6
anjalisara <anjalisaramarkose@gmail.com>
2021-11-12 00:38:58 +0530

    changes to creating_kodak

--- kodak_tensorflow/creating_kodak.py
+++ kodak_tensorflow/creating_kodak.py
@@ -10,19 +10,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Creates the Kodak test set.')
     parser.parse_args()
     
-    path_to_kodak = 'datasets/kodak/results/kodak.npy'
-    path_to_list_rotation = 'datasets/kodak/results/list_rotation.pkl'
+    path_to_kodak = '/kaggle/working/Autoencoder/kodak_tensorflow/datasets/kodak/results/kodak.npy'
+    path_to_list_rotation = '/kaggle/working/Autoencoder/kodak_tensorflow/datasets/kodak/results/list_rotation.pkl'
     
     datasets.kodak.kodak.create_kodak('http://r0k.us/graphics/kodak/kodak/',
-                                      'datasets/kodak/data',
+                                      '/kaggle/temp/Autoencoder/kodak_tensorflow/datasets/kodak/data',
                                       path_to_kodak,
                                       path_to_list_rotation)
     
     # Two luminance images from the Kodak test set are checked visually.
     reference_uint8 = numpy.load(path_to_kodak)
-    tls.save_image('datasets/kodak/visualization/luminance_3.png',
+    tls.save_image('/kaggle/working/Autoencoder/kodak_tensorflow/datasets/kodak/visualization/luminance_3.png',
                    reference_uint8[2, :, :])
-    tls.save_image('datasets/kodak/visualization/luminance_11.png',
+    tls.save_image('/kaggle/working/Autoencoder/kodak_tensorflow/datasets/kodak/visualization/luminance_11.png',
                    reference_uint8[10, :, :])
 
 
